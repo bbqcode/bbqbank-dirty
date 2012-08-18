c4f7a38ba7e71cf97e2729f36aab1b35c5954b91
Alexis L-c <alexisl-c@hotmail.com>
2012-08-18 15:03:46 -0400

    Added model, migration and database

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using bbqbank.Models;
+using bbqbank.ViewModels;
 
 namespace bbqbank.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
+        private readonly IDatabaseContext _context;
+
+        public HomeController(IDatabaseContext context)
+        {
+            _context = context;
+        }
 
         public ActionResult Index()
         {
-            return View();
-        }
+            var bills = _context.Bills.Include(b => b.Items).ToList();
 
+            var alexisPaid = bills.Where(b => b.WhoPaid == Roommate.Alexis).Sum(b => b.Total);
+/*
+            decimal total;
+            foreach(var items in bills.Where(b => b.Items.))
+
+            var alexisUsed = bills.Sum(b => b.Items.Where(i => i.HasAlexisUsed))*/
+            var viewModel = new IndexViewModel
+                                           {
+                                               
+                                           };
+
+            return View(viewModel);
+        }
     }
 }
